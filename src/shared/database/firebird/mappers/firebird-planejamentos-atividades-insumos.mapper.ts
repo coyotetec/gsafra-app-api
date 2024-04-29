f6c74292e2d6d050ab8c956a87e9da6e4e993c55
Iran Adryan <contatoiranadryan@gmail.com>
2024-04-29 15:45:07 -0300

    feat: implements planejamentos atividades insumos resources

---
+++ src/shared/database/firebird/mappers/firebird-planejamentos-atividades-insumos.mapper.ts
@@ -0,0 +1,23 @@
+import { PlanejamentoAtividadeInsumo } from 'src/modules/planejamentos-atividades-insumos/entities/planejamento-atividade-insumo.entity';
+
+interface FirebirdPlanejamentoAtividadeInsumo {
+  ID: number;
+  ID_PLAN_ATV: number;
+  ID_PRODUTO_ALMOXARIFADO: number;
+  ID_UNIDADE: number;
+  QTDE: number;
+}
+
+export class FirebirdPlanejamentosAtividadesInsumosMapper {
+  static toDomain(
+    raw: FirebirdPlanejamentoAtividadeInsumo,
+  ): PlanejamentoAtividadeInsumo {
+    return new PlanejamentoAtividadeInsumo({
+      id: raw.ID,
+      idPlanAtv: raw.ID_PLAN_ATV,
+      idProdutoAlmoxarifado: raw.ID_PRODUTO_ALMOXARIFADO,
+      idUnidade: raw.ID_UNIDADE,
+      qtde: raw.QTDE,
+    });
+  }
+}
