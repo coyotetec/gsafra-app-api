d97ad240dce6979cf249f686b9fcc6cdccc51c1e
Iran Adryan <contatoiranadryan@gmail.com>
2024-05-24 18:07:14 -0300

    feat: add id to indicador totalizer

--- src/modules/dashboard-indicadores/entities/financeiro-view-totalizer-data.entity.ts
+++ src/modules/dashboard-indicadores/entities/financeiro-view-totalizer-data.entity.ts
@@ -1,10 +1,12 @@
 export class FinanceiroViewTotalizerData {
+  id: number;
   name: string;
   error?: string;
   total?: number;
   format?: number;
 
   constructor(props: FinanceiroViewTotalizerData) {
+    this.id = props.id;
     this.name = props.name;
     this.error = props.error;
     this.total = props.total;
